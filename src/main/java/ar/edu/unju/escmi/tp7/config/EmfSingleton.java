bb32a78a4b69a3b5887e25c37aa12a19faae4a02
mateoo <mateoo@DESKTOP-U5ED9AM>
2024-11-02 22:39:22 -0300

    Inicializacion del proyecto

---
+++ src/main/java/ar/edu/unju/escmi/tp7/config/EmfSingleton.java
@@ -0,0 +1,24 @@
+package ar.edu.unju.escmi.tp7.config;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class EmfSingleton {
+	private static EmfSingleton miInstancia = new EmfSingleton();
+	private static final String PERSISTENCE_UNIT_NAME = "Persistence";
+	private EntityManagerFactory emf = null;
+	
+	private EmfSingleton() {
+		
+	}
+	
+	public static EmfSingleton getInstance() {
+		return miInstancia;
+	}
+	
+	public EntityManagerFactory getEmf() {
+		if(this.emf == null) 
+			this.emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+		return this.emf;
+	}
+}
---
+++ src/main/java/ar/edu/unju/escmi/tp7/main/Main.java
@@ -0,0 +1,10 @@
+package ar.edu.unju.escmi.tp7.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
