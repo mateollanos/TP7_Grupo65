9512cbdcadcd3129cd72300e5061373028946a97
Martin Barconte <barcontemartin12@gmail.com>
2024-11-12 18:57:26 -0300

    Arreglos (Factura, IFacturaDao, FacturaDaoImp). BarconteMartin

--- src/main/java/ar/edu/unju/escmi/tp7/dao/imp/FacturaDaoImp.java
+++ src/main/java/ar/edu/unju/escmi/tp7/dao/imp/FacturaDaoImp.java
@@ -6,26 +6,23 @@ import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 
 import ar.edu.unju.escmi.tp7.config.EmfSingleton;
-import ar.edu.unju.escmi.tp7.dao.FacturaDAO;
+import ar.edu.unju.escmi.tp7.dao.IFacturaDao;
 import ar.edu.unju.escmi.tp7.entities.Factura;
 
-public class ImpFacturaDAO implements FacturaDAO {
+public class FacturaDaoImp implements IFacturaDao {
 
 	private static EntityManager manager = EmfSingleton.getInstance().getEmf().createEntityManager();
-	
 	@Override
 	public void guardarFactura(Factura factura) {
 		try {
 			manager.getTransaction().begin();
-			manager.persist(factura);
+			manager.merge(factura);
 			manager.getTransaction().commit();
 		}catch(Exception e) {
 			if(manager.getTransaction() != null) {
 				manager.getTransaction().rollback();
 			}
 			System.out.println("No se pudo guardar la factura");	
-		}finally {
-			manager.close();
 		}
 	}
 
@@ -38,38 +35,51 @@ public class ImpFacturaDAO implements FacturaDAO {
 	public void eliminarFacturaLogicamente(Factura factura) {
 		try {
 			manager.getTransaction().begin();
-			manager.remove(factura);
+			manager.merge(factura);
 			manager.getTransaction().commit();
 		}catch(Exception e) {
 			if(manager.getTransaction() != null) {
 				manager.getTransaction().rollback();
 			}
 			System.out.println("No se pudo eliminar la factura");
-		}finally {
-			manager.close();
 		}
 	}
 
+	@Override
+    public void modificarFactura(Factura factura) {
+        try {
+            manager.getTransaction().begin();
+            manager.merge(factura);
+            manager.flush();
+            manager.refresh(factura);
+            manager.getTransaction().commit();
+        } catch (Exception e) {
+            if (manager.getTransaction() != null) {
+                manager.getTransaction().rollback(); 
+            }
+            System.out.println("No se pudo modificar la factura");
+        }
+    }
+	
 	@Override
 	public void mostrarTodasLasFacturas() {
 		TypedQuery<Factura> query = manager.createQuery("SELECT e FROM Factura e",Factura.class);
 		List<Factura> facturas = query.getResultList();
 		for(Factura factura : facturas) {
-			factura.mostrarFactura();
+			if(factura.isEstado()) {
+				factura.mostrarFactura();
+			}
 		}
 	}
 
 	@Override
 	public void mostrarFacturasFiltradas() {
-	    TypedQuery<Factura> query = manager.createQuery("SELECT f FROM Factura f WHERE f.total >= :monto", Factura.class);
-	    query.setParameter("monto", 500000.0); 
-	    List<Factura> facturas = query.getResultList();
-	    
-	    for (Factura factura : facturas) {
-	        factura.mostrarFactura(); 
-	    }
+		TypedQuery<Factura> query = manager.createQuery("SELECT e FROM Factura e",Factura.class);
+		List<Factura> facturas = query.getResultList();
+		for(Factura factura : facturas) {
+			if(factura.getTotal()>=500000 && factura.isEstado()) {
+				factura.mostrarFactura();
+			}
+		}
 	}
-
-
-
 }
--- src/main/java/ar/edu/unju/escmi/tp7/entities/Factura.java
+++ src/main/java/ar/edu/unju/escmi/tp7/entities/Factura.java
@@ -2,106 +2,114 @@ package ar.edu.unju.escmi.tp7.entities;
 
 import java.time.LocalDate;
 import java.util.List;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "Facturas")
 public class Factura {
-    
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "fact_id")
-    private long id; 
-    
-    @Column(name = "fact_fecha")
-    private LocalDate fecha = LocalDate.now();
-    
-    @ManyToOne
-    @JoinColumn(name = "cliente_id", nullable = false) 
-    private Cliente cliente;
-    
-    @Column(name = "fact_total")
-    private double total;
-    
-    @Column(name = "fact_estado")
-    private boolean estado;
-    
-    @OneToMany(mappedBy = "factura", cascade = CascadeType.ALL)
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "fact_id")
+	private long id; 
+	
+	@Column(name = "fact_fecha")
+	private LocalDate fecha = LocalDate.now();
+	
+	@ManyToOne
+	@JoinColumn(name = "cliente_id")
+	private Cliente cliente; 
+	
+	@Column(name = "fact_total")
+	private double total;
+	
+	@Column(name = "fact_estado")
+	private boolean estado = true;
+	
+	@OneToMany(mappedBy = "factura", cascade = CascadeType.ALL)
     private List<DetalleFactura> detalles;
-    
-    public Factura(LocalDate fecha, Cliente cliente, double total) {
-        super();
-        this.fecha = fecha;
-        this.cliente = cliente;
-        this.total = total;
-        this.estado = true;
-    }
-    
-    public Factura() {}
+	
+	public Factura(Cliente cliente,List<DetalleFactura> detalles) {
+		super();
+		this.cliente = cliente;
+		this.detalles = detalles;
+	}
+		
+	public void setId(long id) {
+		this.id = id;
+	}
 
-    public long getId() {
-        return id;
-    }
+	public List<DetalleFactura> getDetalles() {
+		return detalles;
+	}
 
-    public LocalDate getFecha() {
-        return fecha;
-    }
+	public void setDetalles(List<DetalleFactura> detalles) {
+		this.detalles = detalles;
+	}
+	
+	public Factura() {
+		// TODO Auto-generated constructor stub
+	}
 
-    public void setFecha(LocalDate fecha) {
-        this.fecha = fecha;
-    }
 
-    public Cliente getCliente() {
-        return cliente;
-    }
+	public long getId() {
+		return id;
+	}
+	
+	public LocalDate getFecha() {
+		return fecha;
+	}
+	public void setFecha(LocalDate fecha) {
+		this.fecha = fecha;
+	}
+	
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	
+	public double getTotal() {
+		return total;
+	}
+	public void setTotal(double total) {
+		this.total = total;
+	}
+	
+	public boolean isEstado() {
+		return estado;
+	}
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+	
+	
+	public void mostrarFactura() {
+		System.out.println("\nID: "+id);
+		System.out.println("Fecha: "+fecha);
+		System.out.println("Cliente: "+cliente.getNombre()+" "+cliente.getApellido());
+		System.out.println("Domicilio del cliente: "+cliente.getDomicilio());
+		System.out.println("Monto total: "+total);
+		
+		for(DetalleFactura detalle : detalles) {
+			System.out.println("\nProducto: "+detalle.getProducto().getDescripcion());
+			System.out.println("Precio: "+detalle.getProducto().getPrecioUnitario());
+		}
+	}
 
-    public void setCliente(Cliente cliente) {
-        this.cliente = cliente;
-    }
 
-    public double getTotal() {
-        return total;
-    }
-
-    public void setTotal(double total) {
-        this.total = total;
-    }
-
-    public boolean isEstado() {
-        return estado;
-    }
-
-    public void setEstado(boolean estado) {
-        this.estado = estado;
-    }
-
-    public List<DetalleFactura> getDetalles() {
-        return detalles;
-    }
-
-    public void setDetalles(List<DetalleFactura> detalles) {
-        this.detalles = detalles;
-    }
-    
-    public void mostrarFactura() {
-        System.out.println("\nID: "+id);
-        System.out.println("Fecha: "+fecha);
-        System.out.println("Cliente: "+cliente.getNombre() + " " + cliente.getApellido());
-        System.out.println("Domicilio del cliente: "+cliente.getDomicilio());
-        System.out.println("Monto total: "+total);
-    }
-
-    public void calcularTotal() {
+	public void calcularTotal() {
         this.total = 0.0;
         
         if (detalles != null) {
