ec4567dd03aacbdfd06417e40087abe11e4b9fb5
wiber <wiber@DESKTOP-29TD6KD>
2024-11-12 19:15:18 -0300

    ClienteDaoImp, IclienteDao, Cliente - CambiosNecesarios_Altamirano

--- src/main/java/ar/edu/unju/escmi/tp7/dao/IClienteDao.java
+++ src/main/java/ar/edu/unju/escmi/tp7/dao/IClienteDao.java
@@ -2,10 +2,9 @@ package ar.edu.unju.escmi.tp7.dao;
 
 import ar.edu.unju.escmi.tp7.entities.Cliente;
 
-public interface ClienteDAO {
+public interface IClienteDao {
 	public void guardarCliente(Cliente cliente);
 	public void modificarCliente(Cliente cliente);
 	public Cliente buscarClientePorId(long idCliente);
-	public void borrarCliente(Cliente cliente);
 	public void mostrarTodosLosClientes();
 }
