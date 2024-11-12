a18ba7c6fa10043b202be26d0ab095ded311406c
Martin Barconte <barcontemartin12@gmail.com>
2024-11-12 19:28:13 -0300

    Arreglos(Factura, IFacturaDAO, FacturaDaoImp). BarconteMartin

--- src/main/java/ar/edu/unju/escmi/tp7/dao/IFacturaDao.java
+++ src/main/java/ar/edu/unju/escmi/tp7/dao/IFacturaDao.java
@@ -2,11 +2,13 @@ package ar.edu.unju.escmi.tp7.dao;
 
 import ar.edu.unju.escmi.tp7.entities.Factura;
 
-public interface FacturaDAO {
+public interface IFacturaDao {
+	
 	public void guardarFactura(Factura factura);
 	public Factura buscarFacturaPorId(long idFactura);
-	public void eliminarFacturaLogicamente(Factura factura);
+	public void eliminarFacturaLogicamente(Factura Sfactura);
 	public void mostrarTodasLasFacturas();
 	public void mostrarFacturasFiltradas();
+	public void modificarFactura(Factura factura);
+	
 }
-
