e38abb4788ffbf39d06c4a4c934c48c5f95e5090
mateoo <mateoo@DESKTOP-U5ED9AM>
2024-11-12 18:36:41 -0300

    cambios de configuracion de bd

--- src/main/java/ar/edu/unju/escmi/tp7/config/EmfSingleton.java
+++ src/main/java/ar/edu/unju/escmi/tp7/config/EmfSingleton.java
@@ -5,21 +5,21 @@ import javax.persistence.Persistence;
 
 public class EmfSingleton {
 
-    private static EmfSingleton miInstancia = new EmfSingleton();
-    private static final String PERSISTENCE_UNIT_NAME = "myJpaUnit";  
-    private EntityManagerFactory emf = null;
-    
-    private EmfSingleton() {
-        
-    }
-    
-    public static EmfSingleton getInstance() {
-        return miInstancia;
-    }
-    
-    public EntityManagerFactory getEmf() {
-        if (this.emf == null) 
-            this.emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
-        return this.emf;
-    }
+	private static EmfSingleton miInstancia = new EmfSingleton();
+	private static final String PERSISTENCE_UNIT_NAME = "TestPersistence";
+	private EntityManagerFactory emf = null;
+	
+	private EmfSingleton() {
+		
+	}
+	
+	public static EmfSingleton getInstance() {
+		return miInstancia;
+	}
+	
+	public EntityManagerFactory getEmf() {
+		if(this.emf == null) 
+			this.emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+		return this.emf;
+	}
 }
